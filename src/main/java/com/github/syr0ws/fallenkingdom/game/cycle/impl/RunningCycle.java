6bf9354ea606b4c8ffbd0b639cfe8f556301583e
cleme <clement.gho@gmail.com>
2021-05-09 17:19:50 +0200

    Adding listeners.

--- src/main/java/com/github/syr0ws/fallenkingdom/game/cycle/impl/RunningCycle.java
+++ src/main/java/com/github/syr0ws/fallenkingdom/game/cycle/impl/RunningCycle.java
@@ -1,22 +1,23 @@
 package com.github.syr0ws.fallenkingdom.game.cycle.impl;
 
+import com.github.syr0ws.fallenkingdom.display.types.Message;
 import com.github.syr0ws.fallenkingdom.game.cycle.GameCycle;
 import com.github.syr0ws.fallenkingdom.game.model.GameModel;
 import com.github.syr0ws.fallenkingdom.teams.Team;
 import com.github.syr0ws.fallenkingdom.teams.TeamBase;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,31 +26,67 @@ public class RunningCycle extends GameCycle {
 
     private final Plugin plugin;
     private final GameModel model;
-    private final Listener listener;
 
     public RunningCycle(GameModel model, Plugin plugin) {
         this.plugin = plugin;
         this.model = model;
-        this.listener = new CycleListener();
     }
 
     @Override
     public void start() {
 
-        PluginManager manager = Bukkit.getPluginManager();
-        manager.registerEvents(this.listener, this.plugin);
+        super.addListener(new PlayerListener());
+        super.addListener(new BlockListener());
+        super.registerListeners(this.plugin);
     }
 
     @Override
     public void stop() {
 
-        HandlerList.unregisterAll(this.listener);
+        super.unregisterListeners();
+        super.clearListeners();
     }
 
-    private class CycleListener implements Listener {
+    private class PlayerListener implements Listener {
 
         @EventHandler
-        public void onBlockPlace(BlockPlaceEvent event) {
+        public void onPlayerDamageByPlayer(EntityDamageByEntityEvent event) {
+
+            Entity victim = event.getEntity();
+            Entity damager = event.getDamager();
+
+            if(!(victim instanceof Player && damager instanceof Player)) return;
+
+            if(!model.isPvPEnabled()) {
+
+                event.setCancelled(true);
+
+                new Message("").displayTo(damager); // TODO Send a message here.
+            }
+        }
+    }
+
+    private class BlockListener implements Listener {
+
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onNotAllowedBlockPlace(BlockPlaceEvent event) {
+
+            Player player = event.getPlayer();
+            Block block = event.getBlockPlaced();
+
+            boolean allowed = this.canBePlaced(block);
+
+            // If the block isn't allowed, cancelling the event.
+            if(!allowed) {
+
+                event.setCancelled(true);
+
+                new Message("").displayTo(player); // TODO Send a message here.
+            }
+        }
+
+        @EventHandler(priority = EventPriority.LOW)
+        public void onBlockPlaceInsideBase(BlockPlaceEvent event) {
 
             Player player = event.getPlayer();
             Block block = event.getBlockPlaced();
@@ -57,7 +94,6 @@ public class RunningCycle extends GameCycle {
 
             Optional<Team> optional = model.getTeam(player);
 
-            // Should not happen because all the players which can place blocks are in a team.
             if(!optional.isPresent()) {
                 event.setCancelled(true);
                 return;
@@ -66,27 +102,36 @@ public class RunningCycle extends GameCycle {
             Team team = optional.get();
             TeamBase base = team.getBase();
 
-            // Checking if the placed block is in the base of the player.
-            // If true, allow placing.
+            // Checking if the placed block is in the base of the player. If true, allow placing.
             if(base.getCuboid().isIn(location)) return;
 
-            boolean allowed = this.canBePlaced(block);
+            // Players cannot place allowed blocs inside enemy bases while assaults are not enabled.
+            if(!model.areAssaultsEnabled() && model.isInsideEnemyBase(player)) {
 
-            // If the block isn't allowed, cancelling the event.
-            if(!allowed) event.setCancelled(true);
+                event.setCancelled(true);
+
+                new Message("").displayTo(player); // TODO Send a message here.
+            }
         }
 
-        @EventHandler
+        @EventHandler(priority = EventPriority.LOWEST)
         public void onBlockBreak(BlockBreakEvent event) {
 
             Player player = event.getPlayer();
 
-            // Cancelling the event if the player is inside an enemy base.
-            if(model.isInsideEnemyBase(player)) event.setCancelled(true);
+            // Cancelling the event if the player is trying to break a block inside an enemy base.
+            if(model.isInsideEnemyBase(player)) {
+
+                event.setCancelled(true);
+
+                new Message("").displayTo(player); // TODO Send a message here.
+            }
         }
 
-        @EventHandler
+        @EventHandler(priority = EventPriority.LOWEST)
         public void onBlockExplode(BlockExplodeEvent event) {
+
+            event.blockList().clear();
 
             // Cancelling block explosions if assaults are not enabled.
             if(!model.areAssaultsEnabled()) event.setCancelled(true);
