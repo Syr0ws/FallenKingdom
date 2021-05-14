edc3d2da093cdc6a75ce201a3b04b767672e8503
cleme <clement.gho@gmail.com>
2021-05-14 13:12:22 +0200

    GameCycle is now an AttributeObservable.

--- src/main/java/com/github/syr0ws/fallenkingdom/game/model/cycle/GameCycle.java
+++ src/main/java/com/github/syr0ws/fallenkingdom/game/model/cycle/GameCycle.java
@@ -1,10 +1,20 @@
 package com.github.syr0ws.fallenkingdom.game.model.cycle;
 
+import com.github.syr0ws.fallenkingdom.attributes.Attribute;
+import com.github.syr0ws.fallenkingdom.attributes.AttributeObservable;
+import com.github.syr0ws.fallenkingdom.attributes.AttributeObserver;
 import com.github.syr0ws.fallenkingdom.game.model.GameState;
+import com.github.syr0ws.fallenkingdom.game.model.attributes.GameCycleAttribute;
 
-public abstract class GameCycle {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class GameCycle implements AttributeObservable {
 
     private boolean finished;
+    private final List<AttributeObserver> observers = new ArrayList<>();
 
     public abstract void start();
 
@@ -12,8 +22,31 @@ public abstract class GameCycle {
 
     public abstract GameState getState();
 
+    @Override
+    public void notifyChange(Attribute attribute) {
+        new ArrayList<>(this.observers).stream()
+                .filter(observer -> observer.observed().contains(attribute))
+                .forEach(observer -> observer.onUpdate(attribute));
+    }
+
+    @Override
+    public void addObserver(AttributeObserver observer) {
+        if(!this.observers.contains(observer)) this.observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(AttributeObserver observer) {
+        this.observers.remove(observer);
+    }
+
+    @Override
+    public Collection<AttributeObserver> getObservers() {
+        return Collections.unmodifiableList(this.observers);
+    }
+
     public void finish() {
         this.finished = true;
+        this.notifyChange(GameCycleAttribute.FINISH_STATE);
     }
 
     public boolean isFinished() {
