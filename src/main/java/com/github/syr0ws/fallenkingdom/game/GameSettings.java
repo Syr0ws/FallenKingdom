b566faa1881ede862bd017a1cf787f7d607f7240
cleme <clement.gho@gmail.com>
2021-05-15 18:04:06 +0200

    Adding settings and using SettingKey interface.

--- src/main/java/com/github/syr0ws/fallenkingdom/game/GameSettings.java
+++ src/main/java/com/github/syr0ws/fallenkingdom/game/GameSettings.java
@@ -1,19 +1,36 @@
 package com.github.syr0ws.fallenkingdom.game;
 
 import com.github.syr0ws.fallenkingdom.settings.Setting;
+import com.github.syr0ws.fallenkingdom.settings.SettingKey;
 import com.github.syr0ws.fallenkingdom.settings.impl.LocationSetting;
 import com.github.syr0ws.fallenkingdom.settings.impl.MaterialSetting;
 import com.github.syr0ws.fallenkingdom.settings.impl.SimpleConfigSetting;
+import org.bukkit.Material;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
-public enum GameSettings {
+public enum GameSettings implements SettingKey {
 
-    PVP_ACTIVATION_TIME(new SimpleConfigSetting<>("pvp-activation-time", 1800, Integer.class, time -> time > 0)),
-    ASSAULTS_ACTIVATION_TIME(new SimpleConfigSetting<>("assaults-activation-time", 3600, Integer.class, time -> time > 0)),
-    ALLOWED_BLOCKS(new MaterialSetting("allowed-blocks", new ArrayList<>())),
-    SPAWN_LOCATION(new LocationSetting("spawn-location", Objects::nonNull));
+    PVP_ACTIVATION_TIME(
+            new SimpleConfigSetting<>("pvp-activation-time", 1800, Integer.class, time -> time > 0)),
+
+    ASSAULTS_ACTIVATION_TIME(
+            new SimpleConfigSetting<>("assaults-activation-time", 3600, Integer.class, time -> time > 0)),
+
+    MAX_GAME_DURATION(
+            new SimpleConfigSetting<>("max-game-duration", 10800, Integer.class, duration -> duration > 0)),
+
+    ALLOWED_BLOCKS(
+            new MaterialSetting("allowed-blocks", new ArrayList<>())),
+
+    VAULT_BLOCKS(
+            new MaterialSetting("vault-blocks", Arrays.asList(Material.CHEST, Material.ENDER_CHEST))
+    ),
+
+    SPAWN_LOCATION(
+            new LocationSetting("game-spawn", Objects::nonNull));
 
     private final Setting<?> setting;
 
@@ -23,5 +40,10 @@ public enum GameSettings {
 
     public Setting<?> getSetting() {
         return this.setting;
+    }
+
+    @Override
+    public String getKey() {
+        return this.name();
     }
 }
