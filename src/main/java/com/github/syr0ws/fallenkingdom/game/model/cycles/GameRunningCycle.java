1c01b69af68ffee476f24fa01d2b615c4e90a9a2
cleme <clement.gho@gmail.com>
2021-07-08 00:12:49 +0200

    Adding running task.

--- src/main/java/com/github/syr0ws/fallenkingdom/game/model/cycles/GameRunningCycle.java
+++ src/main/java/com/github/syr0ws/fallenkingdom/game/model/cycles/GameRunningCycle.java
@@ -3,15 +3,21 @@ package com.github.syr0ws.fallenkingdom.game.model.cycles;
 import com.github.syr0ws.fallenkingdom.FKGame;
 import com.github.syr0ws.fallenkingdom.game.controller.FKController;
 import com.github.syr0ws.fallenkingdom.game.model.FKModel;
+import com.github.syr0ws.fallenkingdom.game.model.settings.SettingAccessor;
 import com.github.syr0ws.fallenkingdom.listeners.GameBlockListener;
 import com.github.syr0ws.fallenkingdom.listeners.GamePlayerListener;
+import com.github.syr0ws.universe.Game;
 import com.github.syr0ws.universe.game.model.cycle.GameCycle;
+import com.github.syr0ws.universe.game.model.cycle.GameCycleTask;
 import com.github.syr0ws.universe.listeners.ListenerManager;
+import com.github.syr0ws.universe.tools.Task;
 
 public class GameRunningCycle extends GameCycle {
 
     private final FKController controller;
     private final FKModel model;
+
+    private Task task;
 
     public GameRunningCycle(FKGame game, FKController controller, FKModel model) {
         super(game);
@@ -49,6 +55,22 @@ public class GameRunningCycle extends GameCycle {
     }
 
     @Override
+    public void start() {
+        super.start();
+
+        // Starting task.
+        this.startTask();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+
+        // Stopping task.
+        this.stopTask();
+    }
+
+    @Override
     public FKGame getGame() {
         return (FKGame) super.getGame();
     }
@@ -59,5 +81,43 @@ public class GameRunningCycle extends GameCycle {
 
         manager.addListener(new GamePlayerListener(this.getGame()));
         manager.addListener(new GameBlockListener(this.getGame()));
+    }
+
+    private void startTask() {
+
+        SettingAccessor settings = this.model.getSettings();
+        int duration = settings.getMaxGameDurationSetting().getValue();
+
+        this.task = new RunningCycleTask(this.getGame(), duration);
+        this.task.start();
+    }
+
+    private void stopTask() {
+        this.task.cancel();
+        this.task = null; // Avoid reuse.
+    }
+
+    private class RunningCycleTask extends GameCycleTask {
+
+        private final int duration;
+
+        public RunningCycleTask(Game game, int duration) {
+            super(game);
+
+            if(duration < 0)
+                throw new IllegalArgumentException("Duration must be positive.");
+
+            this.duration = duration;
+        }
+
+        @Override
+        public void run() {
+
+            if(GameRunningCycle.this.model.getTime() > this.duration) {
+
+                GameRunningCycle.this.done();
+
+            } else GameRunningCycle.this.model.addTime();
+        }
     }
 }
