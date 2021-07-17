c813ffda7484dcb17aa0fc53cfa6adaba73d0cfc
cleme <clement.gho@gmail.com>
2021-07-17 20:26:06 +0200

    Adding utils methods.

--- src/main/java/com/github/syr0ws/fallenkingdom/utils/DisplayUtils.java
+++ src/main/java/com/github/syr0ws/fallenkingdom/utils/DisplayUtils.java
@@ -1,17 +1,22 @@
 package com.github.syr0ws.fallenkingdom.utils;
 
 import com.github.syr0ws.universe.Game;
+import com.github.syr0ws.universe.displays.Display;
 import com.github.syr0ws.universe.displays.DisplayFactory;
 import com.github.syr0ws.universe.displays.DisplayManager;
 import com.github.syr0ws.universe.displays.dao.ConfigDisplayDAO;
 import com.github.syr0ws.universe.displays.dao.DisplayDAO;
 import com.github.syr0ws.universe.displays.impl.SimpleDisplayFactory;
 import com.github.syr0ws.universe.displays.impl.SimpleDisplayManager;
+import com.github.syr0ws.universe.displays.types.TextDisplay;
 import com.github.syr0ws.universe.modules.ModuleEnum;
 import com.github.syr0ws.universe.modules.ModuleService;
 import com.github.syr0ws.universe.modules.lang.LangModule;
 import com.github.syr0ws.universe.modules.lang.LangService;
+import org.bukkit.entity.Player;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 public class DisplayUtils {
@@ -27,5 +32,21 @@ public class DisplayUtils {
         DisplayDAO dao = new ConfigDisplayDAO(factory, game.getConfig());
 
         return new SimpleDisplayManager(dao);
+    }
+
+    public static void addPlaceholders(Collection<Display> displays, Map<String, String> placeholders) {
+        displays.stream()
+                .filter(display -> display instanceof TextDisplay)
+                .map(display -> (TextDisplay) display)
+                .forEach(display -> display.addPlaceholders(placeholders));
+    }
+
+    public static void sendDisplays(Collection<Display> displays, Player player) {
+        displays.forEach(display -> display.displayTo(player));
+    }
+
+    public static void sendDisplays(Collection<Display> displays, Collection<Player> players) {
+        players.forEach(player ->
+                displays.forEach(display -> display.displayTo(player)));
     }
 }
