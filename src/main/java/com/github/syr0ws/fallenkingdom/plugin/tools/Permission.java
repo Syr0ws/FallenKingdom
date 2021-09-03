6f41584b5ab1780f208348fff3f6fdb9fcb57312
Syrows <clement.gho@gmail.com>
2021-09-03 11:28:41 +0200

    Fixing permissions.

--- src/main/java/com/github/syr0ws/fallenkingdom/plugin/tools/Permission.java
+++ src/main/java/com/github/syr0ws/fallenkingdom/plugin/tools/Permission.java
@@ -2,12 +2,12 @@ package com.github.syr0ws.fallenkingdom.plugin.tools;
 
 public enum Permission {
 
-    COMMAND_FK("command.fk"),
-    COMMAND_FK_START("command.fk.start"),
-    COMMAND_FK_STOP("command.fk.stop"),
-    COMMAND_FK_PVP("command.fk.pvp"),
-    COMMAND_FK_ASSAULTS("command.fk.assaults"),
-    COMMAND_FK_TEAM("command.fk.team");
+    COMMAND_FK("command.fallendkingdom"),
+    COMMAND_FK_START("command.fallendkingdom.start"),
+    COMMAND_FK_STOP("command.fallendkingdom.stop"),
+    COMMAND_FK_PVP("command.fallendkingdom.pvp"),
+    COMMAND_FK_ASSAULTS("command.fallendkingdom.assaults"),
+    COMMAND_FK_TEAM("command.fallendkingdom.team");
 
     private final String permission;
 
@@ -16,6 +16,6 @@ public enum Permission {
     }
 
     public String get() {
-        return "fk." + this.permission;
+        return "fallenkingdom." + this.permission;
     }
 }
